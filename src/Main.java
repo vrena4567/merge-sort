e410b21d8355798b0103ebfdb7121e9e94f3bdb9
vrena4567 <v.rena4567@gmail.com>
2023-06-05 19:36:37 +0200

    add isSorted method

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Merge sort");
     }
+    public static boolean isSorted(int[] array) {
+
+        return false;
+    }
 }
